f16aecb874fd1f57245e34e0a045d0cb09de5b10
jhoffmann <jens.hoffmann@lidl.com>
2016-06-30 15:33:40 +0200

    * reg should work.

--- m2-lesson1/src/main/java/com/baeldung/lss/web/controller/RegistrationController.java
+++ m2-lesson1/src/main/java/com/baeldung/lss/web/controller/RegistrationController.java
@@ -25,18 +25,18 @@ class RegistrationController {
         return new ModelAndView("registrationPage", "user", new User());
     }
 
-//    @RequestMapping(value = "user/register")
-//    public ModelAndView registerUser(@Valid final User user, final BindingResult result) {
-//        if (result.hasErrors()) {
-//            return new ModelAndView("registrationPage", "user", user);
-//        }
-//        try {
-//            userService.registerNewUser(user);
-//        } catch (EmailExistsException e) {
-//            result.addError(new FieldError("user", "email", e.getMessage()));
-//            return new ModelAndView("registrationPage", "user", user);
-//        }
-//        return new ModelAndView("redirect:/login");
-//    }
+    @RequestMapping(value = "user/register")
+    public ModelAndView registerUser(@Valid final User user, final BindingResult result) {
+        if (result.hasErrors()) {
+            return new ModelAndView("registrationPage", "user", user);
+        }
+        try {
+            userService.registerNewUser(user);
+        } catch (EmailExistsException e) {
+            result.addError(new FieldError("user", "email", e.getMessage()));
+            return new ModelAndView("registrationPage", "user", user);
+        }
+        return new ModelAndView("redirect:/login");
+    }
 
 }
