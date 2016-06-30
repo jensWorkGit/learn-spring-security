81db86a2db8c640d82b58742765162482b567a9f
jhoffmann <jens.hoffmann@lidl.com>
2016-06-30 16:32:25 +0200

    * added mail support.

--- m2-lesson3/src/main/java/com/baeldung/lss/service/UserService.java
+++ m2-lesson3/src/main/java/com/baeldung/lss/service/UserService.java
@@ -5,27 +5,57 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.baeldung.lss.persistence.UserRepository;
-import com.baeldung.lss.validation.EmailExistsException;
 import com.baeldung.lss.model.User;
+import com.baeldung.lss.model.VerificationToken;
+import com.baeldung.lss.persistence.UserRepository;
+import com.baeldung.lss.persistence.VerificationTokenRepository;
+import com.baeldung.lss.validation.EmailExistsException;
 
 @Service
 @Transactional
 class UserService implements IUserService {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private VerificationTokenRepository verificationTokenRepository;
+
+    //
+
+    @Override
+    public User findUserByEmail(final String email) {
+        return userRepository.findByEmail(email);
+    }
 
     @Override
     public User registerNewUser(final User user) throws EmailExistsException {
         if (emailExist(user.getEmail())) {
             throw new EmailExistsException("There is an account with that email address: " + user.getEmail());
         }
-        return repository.save(user);
+        return userRepository.save(user);
     }
 
-    private boolean emailExist(String email) {
-        final User user = repository.findByEmail(email);
+    @Override
+    public void createVerificationTokenForUser(final User user, final String token) {
+        final VerificationToken myToken = new VerificationToken(token, user);
+        verificationTokenRepository.save(myToken);
+    }
+
+    @Override
+    public VerificationToken getVerificationToken(final String token) {
+        return verificationTokenRepository.findByToken(token);
+    }
+
+    @Override
+    public void saveRegisteredUser(final User user) {
+        userRepository.save(user);
+    }
+
+    //
+
+    private boolean emailExist(final String email) {
+        final User user = userRepository.findByEmail(email);
         return user != null;
     }
 
--- m2-lesson3/src/main/java/com/baeldung/lss/web/controller/RegistrationController.java
+++ m2-lesson3/src/main/java/com/baeldung/lss/web/controller/RegistrationController.java
@@ -2,10 +2,11 @@ package com.baeldung.lss.web.controller;
 
 import com.baeldung.lss.model.User;
 import com.baeldung.lss.model.VerificationToken;
-import com.baeldung.lss.persistence.VerificationTokenRepository;
+import com.baeldung.lss.registration.OnRegistrationCompleteEvent;
 import com.baeldung.lss.service.IUserService;
 import com.baeldung.lss.validation.EmailExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,18 +19,15 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Calendar;
-import java.util.UUID;
 
 @Controller
 class RegistrationController {
 
     @Autowired
-    private IUserService userService;
+    private ApplicationEventPublisher eventPublisher;
 
     @Autowired
-    private VerificationTokenRepository verificationTokenRepository;
-
-    //
+    private IUserService userService;
 
     @RequestMapping(value = "signup")
     public ModelAndView registrationForm() {
@@ -37,39 +35,39 @@ class RegistrationController {
     }
 
     @RequestMapping(value = "user/register")
-    public ModelAndView registerUser(
-            @Valid
-            final User user, final BindingResult result, final HttpServletRequest request) {
+    public ModelAndView registerUser(@Valid final User user, final BindingResult result, final HttpServletRequest request) {
         if (result.hasErrors()) {
             return new ModelAndView("registrationPage", "user", user);
         }
         try {
-            user.setEnabled(false);
-            userService.registerNewUser(user);
+            final User registered = userService.registerNewUser(user);
 
-            // create a unique token
-            final String token = UUID.randomUUID().toString();
-            final VerificationToken myToken = new VerificationToken(token, user);
-            verificationTokenRepository.save(myToken);
-            final String appUrl = "http://" + request.getServerName() + ":" + request.getServerPort() +
-                                  request.getContextPath();
-
-            //sendVerificationEmail(user, token, appUrl);
-
+            final String appUrl = "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+            eventPublisher.publishEvent(new OnRegistrationCompleteEvent(registered, appUrl));
         } catch (EmailExistsException e) {
             result.addError(new FieldError("user", "email", e.getMessage()));
             return new ModelAndView("registrationPage", "user", user);
         }
-        return new ModelAndView("redirect:/login");
+        return new ModelAndView("loginPage");
     }
 
     @RequestMapping(value = "/registrationConfirm")
     public ModelAndView confirmRegistration(final Model model, @RequestParam("token") final String token, final RedirectAttributes redirectAttributes) {
-        final VerificationToken verificationToken = verificationTokenRepository.findByToken(token);
+        final VerificationToken verificationToken = userService.getVerificationToken(token);
+        if (verificationToken == null) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Invalid account confirmation token.");
+            return new ModelAndView("redirect:/login");
+        }
+
         final User user = verificationToken.getUser();
+        final Calendar cal = Calendar.getInstance();
+        if ((verificationToken.getExpiryDate().getTime() - cal.getTime().getTime()) <= 0) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Your registration token has expired. Please register again.");
+            return new ModelAndView("redirect:/login");
+        }
 
         user.setEnabled(true);
-        userService.save(user);
+        userService.saveRegisteredUser(user);
         redirectAttributes.addFlashAttribute("message", "Your account verified successfully");
         return new ModelAndView("redirect:/login");
     }
